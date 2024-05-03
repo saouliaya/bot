9b2cd32efaa0f981e75602f0ca438c571b2e8e72
Aya Saouli <ayasaouli405.gmail.com>
2024-05-03 23:43:31 +0200

    new

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def get_conversational_chain():
 
     Answer:
     """
-    model = ChatGoogleGenerativeAI(model='gemini-pro', temperature=0.3, google_api_key=GOOGLE_API_KEY)
+    model = ChatGoogleGenerativeAI(model='gemini-pro', google_api_key=GOOGLE_API_KEY)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
