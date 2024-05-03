32280007ee082b6608a6854a0028610e0c1ca55a
Aya Saouli <ayasaouli405.gmail.com>
2024-05-03 23:20:03 +0200

    new

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ if "chat_session" not in st.session_state:
 # Display the chat history
 for message in st.session_state.chat_session:
     if not st.session_state.new_chat_clicked:
-        with st.chat_message(translate_role_for_streamlit(message.role)):
+        with st.chat_message(translate_role_for_streamlit(message["role"])):
             st.markdown(message.parts[0].text)
 
 #extract the text from the pdf files
